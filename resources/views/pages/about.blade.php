a9480c272f3857acf36bb6a786740d017c7b78c1
Dev-Int <devint.creation@gmail.com>
2021-03-19 17:55:06 +0100

    refactor: remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About us | Hello City</title>
-    </head>
-    <body>
-        <p>Built with &hearts; by Dev-Int.</p>
-        <p><a href="/">Revenir à l'accueil</a></p>
-
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }}</p>
-        </footer>
-    </body>
-</html>
+@section('content')
+    <p>Built with &hearts; by Dev-Int.</p>
+    <p><a href="/">Revenir à l'accueil</a></p>
+@endsection
